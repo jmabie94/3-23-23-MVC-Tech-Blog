c1cac3731a2f64810d715c7de3ceaaf90cfd4789
Jack Mabie <johnmabie94@gmail.com>
2023-03-26 15:05:01 -0500

    updates after unhelpful tutoring session

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
+const loginRoutes = require('./loginRoutes');
 
 router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
+router.use('/login', loginRoutes);
 
 module.exports = router;
