55a8356fd0c96934a17c5587fb499a9b964c49aa
Jack Mabie <johnmabie94@gmail.com>
2023-04-03 21:35:28 -0500

    stage one of fixing for resubmission: reapproaching controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, BlogPost, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// consolidated all non-api routes to homeRoutes.js, added and fixed some existing routes
 
 /* need similar get route for /blogposts */
 router.get('/', async (req, res) => {
@@ -40,24 +43,103 @@ router.get('/', async (req, res) => {
     }
 });
 
-// route so that you can search for all blogposts by a specific user
-// !!!!!!!!! needs finishing !!!!!!!!
-/* router.get('/user/:id', async (req, res) => {
+// create new blogpost form
+router.get('/newpost', withAuth, async (req, res) => {
     try {
-        const dbUserData = await User.findByPk(req.params.id, {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: ['id', 'name']
+        });
+        res.render('blog', {
+            logged_in: req.session.logged_in,
+            user: userData.dataValues,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// shows all blogposts from logged_in user
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const allUserPosts = await BlogPost.findAll({
+            where: {
+                user_id: req.session.user_id,
+            },
+            attributes: ['id', 'title', 'description', 'created_at', 'user_id'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+        });
+        
+        const blogposts = [];
+
+        if (allUserPosts.length == 1) {
+            const title = allUserPosts[0].dataValues.title;
+            const description = allUserPosts[0].dataValues.description;
+            const date = allUserPosts[0].dataValues.created_at;
+            const postId = allUserPosts[0].dataValues.id;
+            blogposts.push({ postId, title, description, date });
+        } else {
+            allUserPosts.forEach((blogpost) => {
+                const title = blogpost.dataValues.title;
+                const description = blogpost.dataValues.description;
+                const date = blogpost.dataValues.created_at;
+                const postId = blogpost.dataValues.id;
+                blogposts.push({ postId, title, description, date });
+            });
+        }
+
+        blogposts.reverse();
+
+        res.render('dashboard', {
+            blogposts,
+            logged_in: req.session.logged_in,
+            username: req.session.username,
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// route so that you can search for all blogposts by a specific user
+// in theory this should do the trick
+router.get('/user/:id', withAuth, async (req, res) => {
+    try {
+        const userBlogPostData = await User.findByPk(req.params.id, {
             include: [
                 {
                     model: BlogPost,
-                    attributes: ['id', 'title', 'description', 'created_at', 'user_id'],
-                    include: {
-                        model: U
-                    }
-                }
-            ]
-        })
-    }
-}) */
+                    include: [Comment],
+                },
+            ],
+            attributes: ['id', 'username']
+        });
 
+        const userBlogPosts = userBlogPostData.blogposts.map((blogpost) => blogpost.get({ plain: true }));
+        console.log(userBlogPosts);
+
+        res.render('single-user', {
+            userBlogPosts,
+            logged_in: req.session.logged_in,
+            user: userBlogPostData.dataValues,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// loads specifc blogpost based on its ID
 router.get('/blogposts/:id', async (req, res) => {
     try {
         const dbBlogPostData = await BlogPost.findOne({
@@ -121,4 +203,35 @@ router.get('/blogposts/:id', async (req, res) => {
     };
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.redirect('/');
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
 module.exports = router;
+
+// views needed:
+/* 
+main.handlebars
+homepage.handlebars
+dashboard.handlebars
+login.handlebars
+blog.handlebars
+single-post.handlebars
+single-user.handlebars 
+*/
+// all of these need to be created from scratch or redone
--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
-const loginRoutes = require('./loginRoutes');
+/* const dashboardRoutes = require('./dashboardRoutes');
+const loginRoutes = require('./loginRoutes'); */
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/dashboard', dashboardRoutes);
-router.use('/login', loginRoutes);
+// probably need to consolidate all non-api routes to homeRoutes
+/* router.use('/dashboard', dashboardRoutes);
+router.use('/login', loginRoutes); */
 
 module.exports = router;
